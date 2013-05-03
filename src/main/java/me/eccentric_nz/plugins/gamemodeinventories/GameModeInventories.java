2af38b3da04ac3932ff7297c9a2ea739facd107c
eccentric devotion <eccentricdevotion@gmail.com>
2013-05-03 19:18:15 +1200

    Update for Bukkit 1.5.2

--- src/main/java/me/eccentric_nz/plugins/gamemodeinventories/GameModeInventories.java
+++ src/main/java/me/eccentric_nz/plugins/gamemodeinventories/GameModeInventories.java
@@ -17,63 +17,44 @@ public class GameModeInventories extends JavaPlugin implements Listener {
     GameModeInventoriesDeath DeathListener = new GameModeInventoriesDeath(this);
     private GameModeInventoriesCommands commando;
     GameModeInventoriesDatabase service = GameModeInventoriesDatabase.getInstance();
+    boolean found = false;
 
     @Override
     public void onEnable() {
-        String packageName = this.getServer().getClass().getPackage().getName();
-        // Get full package string of CraftServer.
-        // org.bukkit.craftbukkit.versionstring (or for pre-refactor, just org.bukkit.craftbukkit
-        String version = packageName.substring(packageName.lastIndexOf('.') + 1);
-        // Get the last element of the package
-        if (version.equals("craftbukkit")) { // If the last element of the package was "craftbukkit" we are now pre-refactor
-            version = "pre";
-        }
-        try {
-            final Class<?> clazz = Class.forName("me.eccentric_nz.plugins.gamemodeinventories.GameModeInventoriesInventory_" + version);
-            // Check if we have a NMSHandler class at that location.
-            if (GameModeInventoriesInventory_api.class.isAssignableFrom(clazz)) { // Make sure it actually implements IOP
-                this.inventoryHandler = (GameModeInventoriesInventory_api) clazz.getConstructor().newInstance(); // Set our handler
+        if (loadClasses()) {
+            plugin = this;
+            if (!getDataFolder().exists()) {
+                if (!getDataFolder().mkdir()) {
+                    System.err.println(GameModeInventoriesConstants.MY_PLUGIN_NAME + "Could not create directory!");
+                    System.err.println(GameModeInventoriesConstants.MY_PLUGIN_NAME + "Requires you to manually make the GameModeInventories/ directory!");
+                }
+                getDataFolder().setWritable(true);
+                getDataFolder().setExecutable(true);
             }
-        } catch (final Exception e) {
-            this.getLogger().severe("Could not find support for this CraftBukkit version.");
-            this.getLogger().info("Check for updates at http://dev.bukkit.org/server-mods/gamemodeinventories/");
-            this.setEnabled(false);
-            return;
-        }
-        this.getLogger().log(Level.INFO, "Loading support for CB {0}", version);
-        plugin = this;
-        if (!getDataFolder().exists()) {
-            if (!getDataFolder().mkdir()) {
-                System.err.println(GameModeInventoriesConstants.MY_PLUGIN_NAME + "Could not create directory!");
-                System.err.println(GameModeInventoriesConstants.MY_PLUGIN_NAME + "Requires you to manually make the GameModeInventories/ directory!");
+            this.saveDefaultConfig();
+            GameModeInventoriesConfig tc = new GameModeInventoriesConfig(this);
+            tc.checkConfig();
+
+            try {
+                String path = getDataFolder() + File.separator + "GMI.db";
+                service.setConnection(path);
+                service.createTables();
+            } catch (Exception e) {
+                debug("Connection and Tables Error: " + e);
             }
-            getDataFolder().setWritable(true);
-            getDataFolder().setExecutable(true);
+
+            pm.registerEvents(GMListener, this);
+            pm.registerEvents(DeathListener, this);
+            commando = new GameModeInventoriesCommands(plugin);
+            getCommand("gmi").setExecutor(commando);
+
+            try {
+                MetricsLite metrics = new MetricsLite(this);
+                metrics.start();
+            } catch (IOException e) {
+                // Failed to submit the stats :-(
+            }
         }
-        this.saveDefaultConfig();
-        GameModeInventoriesConfig tc = new GameModeInventoriesConfig(this);
-        tc.checkConfig();
-
-        try {
-            String path = getDataFolder() + File.separator + "GMI.db";
-            service.setConnection(path);
-            service.createTables();
-        } catch (Exception e) {
-            debug("Connection and Tables Error: " + e);
-        }
-
-        pm.registerEvents(GMListener, this);
-        pm.registerEvents(DeathListener, this);
-        commando = new GameModeInventoriesCommands(plugin);
-        getCommand("gmi").setExecutor(commando);
-
-        try {
-            MetricsLite metrics = new MetricsLite(this);
-            metrics.start();
-        } catch (IOException e) {
-            // Failed to submit the stats :-(
-        }
-
     }
 
     @Override
@@ -82,7 +63,9 @@ public class GameModeInventories extends JavaPlugin implements Listener {
         try {
             service.connection.close();
         } catch (Exception e) {
-            System.err.println(GameModeInventoriesConstants.MY_PLUGIN_NAME + " Could not close database connection: " + e);
+            if (found) {
+                System.err.println("[GameModeInventories] Could not close database connection: " + e);
+            }
         }
     }
 
@@ -90,5 +73,32 @@ public class GameModeInventories extends JavaPlugin implements Listener {
         if (getConfig().getBoolean("debug") == true) {
             System.out.println("[GameModeInventories Debug] " + o);
         }
+    }
+
+    private boolean loadClasses() {
+        String packageName = this.getServer().getClass().getPackage().getName();
+        // Get full package string of CraftServer.
+        // org.bukkit.craftbukkit.versionstring (or for pre-refactor, just org.bukkit.craftbukkit
+        String version = packageName.substring(packageName.lastIndexOf('.') + 1);
+        // Get the last element of the package
+        if (version.equals("craftbukkit")) { // If the last element of the package was "craftbukkit" we are now pre-refactor
+            version = "pre";
+        }
+
+        try {
+            final Class<?> clazz = Class.forName("me.eccentric_nz.plugins.gamemodeinventories.GameModeInventoriesInventory_" + version);
+            // Check if we have a NMSHandler class at that location.
+            if (GameModeInventoriesInventory_api.class.isAssignableFrom(clazz)) { // Make sure it actually implements IOP
+                this.inventoryHandler = (GameModeInventoriesInventory_api) clazz.getConstructor().newInstance(); // Set our handler
+            }
+            found = true;
+            this.getLogger().log(Level.INFO, "Loading support for CB {0}", version);
+        } catch (final Exception e) {
+            this.getLogger().severe("Could not find support for this CraftBukkit version.");
+            this.getLogger().info("Check for updates at http://dev.bukkit.org/server-mods/gamemodeinventories/");
+            this.setEnabled(false);
+            found = false;
+        }
+        return found;
     }
 }
